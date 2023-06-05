b29a383cffa32ee944b1e10e210f34353fe2b6b9
OhBeomho <tyler0322@naver.com>
2023-06-06 08:59:30 +0900

    t

--- src/router/wrap.ts
+++ src/router/wrap.ts
@@ -4,7 +4,7 @@ export const wrap = (
   asyncFn: (req: Request, res: Response, next?: NextFunction) => Promise<void>
 ) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    res.appendHeader("Access-Control-Allow-Origin", "https://obho-poll.netlify.app");
+    // res.appendHeader("Access-Control-Allow-Origin", "https://obho-poll.netlify.app");
 
     try {
       return await asyncFn(req, res, next);
