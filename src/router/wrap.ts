cd5fcda44d9d2ed280e3dbd3d832eac2ed54533b
OhBeomho <tyler0322@naver.com>
2023-06-06 11:51:48 +0900

    f

--- src/router/wrap.ts
+++ src/router/wrap.ts
@@ -12,14 +12,14 @@ export const wrap = (
       return await asyncFn(req, res, next);
     } catch (error) {
       const errMsg = (error as Error).message;
-      const code = Number(errMsg.substring(0, 3));
+      const code = Number(errMsg.substring(0, 3)) || 500;
       const message = errMsg.substring(4);
       const response = res.status(code);
 
       response.setHeader("Access-Control-Allow-Origin", "https://obho-poll.netlify.app");
       response.setHeader("Access-Control-Allow-Methods", "*");
       response.setHeader("Access-Control-Allow-Headers", "*");
-      response.json({ code, error: message });
+      response.json({ error: message });
     }
   };
 };
