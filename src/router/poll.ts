a3099a7e52d4039c1c718a9acc358424e9c85524
OhBeomho <tyler0322@naver.com>
2023-06-06 09:14:46 +0900

    afds

--- src/router/poll.ts
+++ src/router/poll.ts
@@ -39,6 +39,7 @@ router.post(
     poll.voters.push(ip);
     await poll.save();
 
+    res.setHeader("Access-Control-Allow-Origin", "https://obho-poll.netlify.app");
     res.json({ code: 200 });
   })
 );
